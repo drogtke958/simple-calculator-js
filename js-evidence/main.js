29ead70c0182757d87a4134683ec7c76d43a4ada
Keily <drogtke958@northwested.org>
2024-04-29 11:02:14 -0400

    added files

---
+++ js-evidence/main.js
@@ -0,0 +1,31 @@
+//Keily Drogt
+//April 29 2024
+//Arrays Basics
+
+
+//Input
+
+const firstName = 'Keily';
+const lastName = 'Drogt';
+const date = 'April 29 2024';
+const title = 'Arrays basics';
+
+const contries = ["Japan", "France", "Russia", "Spain"];
+contries.push("UK");
+
+const flavors = ["Stawberry", "Lavender", "Blue Raspberry", "Lemon"];
+flavors.pop("Lemon")
+
+//Process
+
+
+
+//Output
+const myOutput = `
+${firstName} ${lastName}
+${date}
+${title}
+${contries}
+${flavors}`
+
+console.log(myOutput);
---
+++ random-nums-js/main.js
@@ -0,0 +1,34 @@
+//Keily Drogt
+//April 24 2024
+//String Templete Examples
+
+
+const randNum1 = Math.random();
+const randNum2 = Math.random();
+const randNum3 = Math.random();
+
+console.log(`First random number is: ${randNum1}`);
+console.log(`Second random number is: ${randNum2}`);
+console.log(`Third random number is: ${randNum3}`);
+
+
+
+
+const firstRandNum1 = Math.random()*5;
+const secondRandNum2 = Math.random()*5;
+const thirdRandNum3 = Math.random()*5;
+
+console.log(`First random number is: ${firstRandNum1}`)
+console.log(`Second random number is: ${secondRandNum2}`)
+console.log(`Third random number is: ${thirdRandNum3}`)
+
+
+
+
+const random1 = Math.floor(Math.random()*5)
+const random2 = Math.floor(Math.random()*5)
+const random3 = Math.floor(Math.random()*5)
+
+console.log(`First random number is: ${random1}`)
+console.log(`Second random number is: ${random2}`)
+console.log(`Third random number is: ${random3}`)
